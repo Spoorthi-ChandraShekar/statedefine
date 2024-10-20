1e483b7f59234c85decc6cd2a04ea7f5f42e7647
Spoorthi-ChandraShekar <spoorthigowda2k2@gmail.com>
2024-10-20 15:12:04 +0530

    Update statedefine.py

--- statedefine.py
+++ statedefine.py
@@ -5,9 +5,8 @@ class State:
     def __repr__(self):
         return self.name
 
-
 class Transition:
     def __init__(self, from_state, input_symbol, to_state):
         self.from_state = from_state
         self.input_symbol = input_symbol
-        self.to_state = to_state 
+        self.to_state = to_state
